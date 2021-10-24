89f9fc231a5be783593b1362e6360f81545c313b
Aman-Abd <aman.abdyraxmanov@mail.ru>
2021-10-24 23:05:59 +0600

    Fix02

--- src/main/java/com/example/refund/config/SwaggerConfiguration.java
+++ src/main/java/com/example/refund/config/SwaggerConfiguration.java
@@ -18,7 +18,7 @@ public class SwaggerConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.microservice.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.refund.controllers"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
